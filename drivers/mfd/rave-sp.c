57021e4dbffaf357d34acd7447efca920cbb1385
Wladimir J. van der Laan <laanwj@gmail.com>
2017-08-11 07:30:56 +0000

    mfd: shut up "rave-sp rx" "rave-sp tx" messages

--- drivers/mfd/rave-sp.c
+++ drivers/mfd/rave-sp.c
@@ -437,8 +437,10 @@ static int rave_sp_write(struct rave_sp *sp, const u8 *data, u8 data_size)
 
 	length = dest - frame;
 
+#if 0
 	print_hex_dump(KERN_DEBUG, "rave-sp tx: ", DUMP_PREFIX_NONE,
 		       16, 1, frame, length, false);
+#endif
 
 	return serdev_device_write(sp->serdev, frame, length, HZ);
 }
@@ -556,8 +558,10 @@ static void rave_sp_receive_frame(struct rave_sp *sp,
 	struct device *dev           = &sp->serdev->dev;
 	u8 crc_calculated[checksum_length];
 
+#if 0
 	print_hex_dump(KERN_DEBUG, "rave-sp rx: ", DUMP_PREFIX_NONE,
 		       16, 1, data, length, false);
+#endif
 
 	if (unlikely(length <= checksum_length)) {
 		dev_warn(dev, "Dropping short frame\n");
